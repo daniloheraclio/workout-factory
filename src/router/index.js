bc5993f7855330eda0f7994b4bad79f4f396ec12
Danilo Heraclio <emaildedanilo@gmail.com>
2021-10-08 19:03:14 -0400

    added router guard - firebase auth

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,20 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 
+import { auth } from '../firebase';
+
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: () =>
+      import(/* webpackChunkName: "login" */ '../views/Login.vue'),
+    meta: {
+      requiresGuest: true,
+    },
+  },
   {
     path: '/',
     name: 'Home',
@@ -25,6 +36,30 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some((x) => x.meta.requiresAuth);
+  const requiresGuest = to.matched.some((record) => record.meta.requiresGuest);
+
+  if (requiresAuth && !auth.currentUser) {
+    next({
+      path: '/login',
+      query: {
+        redirect: to.fullPath,
+      },
+    });
+  } else if (requiresGuest && auth.currentUser) {
+    next({
+      path: '/',
+      query: {
+        redirect: to.fullPath,
+      },
+    });
+  } else {
+    // Proceed to route
+    next();
+  }
 });
 
 export default router;
