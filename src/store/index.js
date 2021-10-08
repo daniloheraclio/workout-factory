d4f73541f37ecd815dae4672e2dd6f848e360bfb
Danilo Heraclio <emaildedanilo@gmail.com>
2021-10-08 19:08:51 -0400

    WIP store and login page

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,18 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    isLoading: false,
+    userProfile: {},
+  },
+  mutations: {
+    SET_IS_LOADING(state, isLoading) {
+      state.isLoading = isLoading;
+    },
+    SET_USER_PROFILE(state, user) {
+      state.userProfile = user;
+    },
+  },
   actions: {},
   modules: {},
 });
