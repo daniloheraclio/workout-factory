4e27c10a5410501c9c70f2e0b6011b08abfaccef
Danilo Heraclio <emaildedanilo@gmail.com>
2021-11-05 10:09:07 -0400

    added blur to modal bg

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
     <!-- Overlay -->
-    <div class="fixed inset-0 z-20 overflow-y-auto flex justify-center items-center bg-gray-600 bg-opacity-50">
+    <div
+      class="
+        fixed
+        inset-0
+        z-20
+        overflow-y-auto
+        flex
+        justify-center
+        items-center
+        bg-gray-600 bg-opacity-40
+        md:backdrop-filter md:backdrop-blur-sm
+      "
+    >
       <!-- Content -->
       <div class="bg-white p-3 w-full h-full md:w-96 md:h-auto md:m-auto md:p-5 md:rounded-md">
         <slot name="content" />
